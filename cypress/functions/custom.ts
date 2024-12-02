153b32525527363dc9eb6783ba89dbe0a0305cf1
Evzen <evgenii.sitkin@pbktechnology.cz>
2024-12-02 10:21:28 +0100

    deleted unused function

--- cypress/functions/custom.ts
+++ cypress/functions/custom.ts
@@ -3,17 +3,6 @@ export const custom = {
 		return Math.floor(Math.random() * 10) + 1;
 	},
 
-	/*generateRandomString(length: number = 12): string {
-		const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		let result = '';
-		const randomArray = new Uint8Array(length);
-		crypto.getRandomValues(randomArray);
-		randomArray.forEach((number) => {
-			result += chars[number % chars.length];
-		});
-		return result;
-	},*/
-
 	generateRandomString(length: number = 12): string {
 		const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 		let result = '';
